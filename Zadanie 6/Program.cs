475546c2d617327e8a8cd09ad498b5f758c4d52c
4207088 <4207088@TE-GDA-D-D21E08>
2025-03-24 14:24:38 +0100

    Dodaj pliki projektów.

---
+++ Zadanie 6/Program.cs
@@ -0,0 +1,40 @@
+﻿while(true)
+{
+    Console.Clear();
+    Console.WriteLine("1. Dodaj produkt");
+    Console.WriteLine("2. Usun produkt");
+    Console.WriteLine("3. Wyswietl produkty");
+    Console.WriteLine("4. Akutalizuj produkt");
+    Console.WriteLine("5. Oblicz wartość magazynu");
+    Console.WriteLine("6. Wyjście");
+
+    ConsoleKeyInfo key = Console.ReadKey();
+    char choice = key.KeyChar;
+    switch (choice)
+    {
+        case '1':
+            Console.Clear();
+            Console.WriteLine("Podaj nazwę produktu: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Podaj cenę produktu");
+            Console.WriteLine("Podaj kod kreskowy produktu");
+            Console.WriteLine("Podaj ilość produktu");
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
