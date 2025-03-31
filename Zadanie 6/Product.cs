588170e9e036860540c810c0a2ec8bc609353c02
Nikodem Dębowski <nikodem.debowski@teb.edu.pl>
2025-03-31 14:10:46 +0200

    Zrefaktoryzowano zarządzanie produktami
    Usunięto dyrektywy `using` w `Class1.cs` i zdefiniowano klasę `Program` z metodami do zarządzania produktami. W `Program.cs` dodano dyrektywę `using System.Data.SqlTypes`, zainicjowano listę `storage` oraz poprawiono interakcję z użytkownikiem, wprowadzając opcje dodawania, usuwania, wyświetlania, aktualizowania produktów i obliczania wartości magazynu. W `Product.cs` dodano klasę `Product` z odpowiednimi właściwościami i konstruktorem.

---
+++ Zadanie 6/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_6
+{
+    class Product
+    {
+        public string KodKreskowy;
+        public string Nazwa;
+        public double Cena;
+        public float Ilość;
+
+        public Product(string kodKreskowy, string nazwa, double cena, float ilość)
+        {
+            KodKreskowy = kodKreskowy;
+            Nazwa = nazwa;
+            Cena = cena;
+            Ilość = ilość;
+        }
+    }
+}
+
+
--- Zadanie 6/Program.cs
+++ Zadanie 6/Program.cs
@@ -1,6 +1,10 @@
-﻿while(true)
+﻿using System.Data.SqlTypes;
+using Zadanie_6;
+
+List<Product> storage = new List<Product>();
 {
-    Console.Clear();
+    
+    while (true) {
     Console.WriteLine("1. Dodaj produkt");
     Console.WriteLine("2. Usun produkt");
     Console.WriteLine("3. Wyswietl produkty");
@@ -8,33 +12,128 @@
     Console.WriteLine("5. Oblicz wartość magazynu");
     Console.WriteLine("6. Wyjście");
 
-    ConsoleKeyInfo key = Console.ReadKey();
-    char choice = key.KeyChar;
-    switch (choice)
-    {
-        case '1':
-            Console.Clear();
-            Console.WriteLine("Podaj nazwę produktu: ");
-            string name = Console.ReadLine();
-            Console.WriteLine("Podaj cenę produktu");
-            Console.WriteLine("Podaj kod kreskowy produktu");
-            Console.WriteLine("Podaj ilość produktu");
+   
+  
+        ConsoleKeyInfo key = Console.ReadKey();
+        char choice = key.KeyChar;
+        switch (choice)
+        {
+            case '1':
 
+                Console.Clear();
+                Console.WriteLine("Podaj nazwę produktu: ");
+                string name = Console.ReadLine();
+                Console.WriteLine("Podaj cenę produktu");
+                double Cena = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Podaj kod kreskowy produktu");
+                string KodKreskowy = Console.ReadLine();
+                Console.WriteLine("Podaj ilość produktu");
+                float Ilość = Convert.ToSingle(Console.ReadLine());
+                Product newProduct = new Product(KodKreskowy, name, Cena, Ilość);
+                storage.Add(newProduct);
+                Product p = new Product("123456890",
+                                        "Mleko",
+                                        2.5,
+                                        20);
+                storage.Add(p);
+                break;
+            case '2':
+                Console.Clear();
+                Console.WriteLine("Podaj nazwę produktu do usunięcia: ");
+                string search = Console.ReadLine();
+                Product foundProduct = storage.Find(product => product.Nazwa == search);
+                storage.Remove(foundProduct);
+                break;
+            case '3':
+                Console.Clear();
+                foreach (Product product in storage)
+                {
+                    Console.WriteLine(" Product o nazwie: " + product.Nazwa +
+                        " O kodzie kreskowym: " + product.KodKreskowy +
+                        " Kosztuje: " + product.Cena + "zł" +
+                        " Ilość: " + product.Ilość);
+                }
+                break;
+            case '4':
+                Console.Clear();
+                Console.WriteLine("Podaj nazwę produktu do zaktualizowania: ");
+                string search2 = Console.ReadLine();
+                Product foundProduct2 = storage.Find(product => product.Nazwa == search2);
+                Console.WriteLine("Podaj nową cenę produktu");
+                double Cena2 = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine(
+                    "Podaj nowy kod kreskowy produktu");
+                string KodKreskowy2 = Console.ReadLine();
+                Console.WriteLine("Podaj nową ilość produktu");
+                float Ilość2 = Convert.ToSingle(Console.ReadLine());
+                foundProduct2.Cena = Cena2;
+                foundProduct2.KodKreskowy = KodKreskowy2;
+                foundProduct2.Ilość = Ilość2;
+                break;
+
+            case '5':
+                Console.Clear();
+                double totalValue = 0;
+                foreach (Product product in storage)
+                {
+                    totalValue += product.Cena * product.Ilość;
+                }
+                Console.WriteLine("Wartość całego magazynu to: " + totalValue + "zl");
+                break;
+            case '6':
+                Environment.Exit(0);
+                return;
+                break;
+
+
+        }
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
+
+//        p = new Product("123456891",
+//                     "Chleb",
+//                     2.0,
+//                     10);   
+//        storage.Add(p);
+
+//        string search = "Mleko";
+
+//        Product foundProduct = storage.Find(product => product.Nazwa == search);
+
+//        Product foundProduct2 = null;
+
+//        foreach (Product product in storage)
+//        {
+//            if (product.Nazwa == search)
+//            {
+//                foundProduct2 = product;
+//                break;
+//            }
+//        }
+
+//        storage.Remove(foundProduct);
+//        foundProduct2.Cena = 3.0;
+//        foreach (Product product in storage)
+//        {
+//            Console.WriteLine(" Product o nazwie: " + product.Nazwa + 
+//                " O kodzie kreskowym: " + product.KodKreskowy +
+//                " Kosztuje: " + product.Cena + "zł");
+//        }
+
+//        double totalValue = 0;
+//        foreach (Product product in storage)
+//        {
+//            totalValue += product.Cena * product.Ilość;
+//}
+
+//Console.WriteLine("Wartość całego magazynu to: " + totalValue + "zl");
+
+
+
+
+
+
+
+
 
 
